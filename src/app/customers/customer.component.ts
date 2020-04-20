5d40ab9813a06620821c4bd759a71e30c27566a8
fernandesalan23 <fernandes.alan23@gmail.com>
2020-04-21 03:50:55 +0530

    setValue, patchValue, FormBuilder, built-in validation, custom validators with params

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): {[key: string]: boolean} | null => {
+    if(c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)){
+      return { 'range': true };
+    }
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-customer',
@@ -9,15 +18,46 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
+
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', Validators.required, Validators.maxLength(50)],
+      email: ['', Validators.email],
+      phone: '',
+      notification: 'email',
+      rating: [null, ratingRange(1,5)],
+      sendCatalog: true
+    });
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData(): void{
+    this.customerForm.setValue({
+      firstName: "Jack",
+      lastName: 'Harkness',
+      email: 'jack@torchwood.com',
+      sendCatalog: false
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm));
+  }
+
+  setNotification(notifyVia: string): void{
+    const phoneControl = this.customerForm.get('phone');
+    if(notifyVia === 'text'){
+      phoneControl.setValidators(Validators.required);
+    }
+    else{
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
