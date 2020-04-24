c3cd00def04b6c71f4b46833e59e54a89c64334a
fernandesalan23 <fernandes.alan23@gmail.com>
2020-04-25 04:32:27 +0530

    validation

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,16 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
-
-function ratingRange(min: number, max: number): ValidatorFn {
-  return (c: AbstractControl): {[key: string]: boolean} | null => {
-    if(c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)){
-      return { 'range': true };
-    }
-    return null;
-  };
-}
 
 @Component({
   selector: 'app-customer',
@@ -19,35 +10,33 @@ function ratingRange(min: number, max: number): ValidatorFn {
 })
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
-
   customer = new Customer();
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.customerForm = this.fb.group({
-      firstName: ['', [Validators.required, Validators.minLength(3)]],
-      lastName: ['', Validators.required, Validators.maxLength(50)],
-      email: ['', Validators.email],
+      firstName: ['',[Validators.required, Validators.minLength(3)]],
+      lastName: ['',[Validators.required, Validators.maxLength(50)]],
+      email: ['',[Validators.required, Validators.email]],
       phone: '',
       notification: 'email',
-      rating: [null, ratingRange(1,5)],
       sendCatalog: true
-    });
+    })
   }
 
   populateTestData(): void{
-    this.customerForm.setValue({
-      firstName: "Jack",
-      lastName: 'Harkness',
-      email: 'jack@torchwood.com',
+    this.customerForm.patchValue({
+      firstName: 'John',
+      lastName: '',
+      email: 'johndoe@asd.fgh',
       sendCatalog: false
     });
   }
 
   save() {
     console.log(this.customerForm);
-    console.log('Saved: ' + JSON.stringify(this.customerForm));
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 
   setNotification(notifyVia: string): void{
