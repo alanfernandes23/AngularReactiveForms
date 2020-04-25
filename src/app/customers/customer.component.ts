1ae700955e69cb66b67642f51a8f342f06394152
fernandesalan23 <fernandes.alan23@gmail.com>
2020-04-26 04:09:20 +0530

    validation

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,7 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+function emailMatcher(c: AbstractControl): {[key: string]: boolean} {
+  const emailControl = c.get('email');
+  const confirmEmailControl = c.get('confirmEmail');
+
+  if(emailControl.pristine || confirmEmailControl.pristine){
+    return null;
+  }
+  
+  if(emailControl.value === confirmEmailControl.value){
+    return null;
+  }
+  return {'match': true};
+}
+
+function ratingRange(min: number, max: number): ValidatorFn{  
+  return (c: AbstractControl): { [key: string]: boolean} | null => {
+    if(c.value !== null && (isNaN(c.value) || c.value < min || c.value > max )){
+      return {'range': true};
+    }
+    return null;
+  }
+}
 
 @Component({
   selector: 'app-customer',
@@ -18,9 +41,13 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ['',[Validators.required, Validators.minLength(3)]],
       lastName: ['',[Validators.required, Validators.maxLength(50)]],
-      email: ['',[Validators.required, Validators.email]],
+      emailGroup: this.fb.group({
+          email: ['',[Validators.required, Validators.email]],
+          confirmEmail: ['', Validators.required],
+      }, {validator: emailMatcher}),
       phone: '',
       notification: 'email',
+      rating: [null, ratingRange(1,5)],
       sendCatalog: true
     })
   }
